4899ef852be267fdc7ac259f7ebb59372bca1132
payanok17 <payanok17@gmail.com>
2025-07-06 18:07:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ def webhook():
 
 if __name__ == '__main__':
     bot.remove_webhook()
-    bot.set_webhook(url='https://neurox-14ud.onrender.com/' + API_TOKEN)
+    bot.set_webhook(url='https://neurox-webhook-bot.onrender.com/' + API_TOKEN)
     app.run(host='0.0.0.0', port=5000)
