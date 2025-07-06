ab311b27f84c6fe68e9c46cc05c5664b0f546771
payanok17 <payanok17@gmail.com>
2025-07-06 17:45:50 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import telebot
+
+API_TOKEN = '8119510662:AAHCc0qjGqM108LLpbThhJy71c4FeV9iDoU'
+bot = telebot.TeleBot(API_TOKEN)
+app = Flask(__name__)
+
+@app.route('/' + API_TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+@app.route('/')
+def webhook():
+    return 'NeuroX Online'
+
+if __name__ == '__main__':
+    bot.remove_webhook()
+    bot.set_webhook(url='https://neurox-14ud.onrender.com/' + API_TOKEN)
+    app.run(host='0.0.0.0', port=5000)
