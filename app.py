2f2312fd66054c39bee17739b0b6ac8bca0b4881
payanok17 <payanok17@gmail.com>
2025-07-07 13:38:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
 from flask import Flask, request
 import telebot
 import os
+
 API_TOKEN = os.environ.get("API_TOKEN")
 bot = telebot.TeleBot(API_TOKEN)
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def getMessage():
+@app.route(f'/{API_TOKEN}', methods=['POST'])
+def receive_update():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
-    return "!", 200
+    return "OK", 200
 
 @app.route('/')
-def webhook():
-    return 'NeuroX Online'
-
-if __name__ == '__main__':
-    bot.remove_webhook()
-    bot.set_webhook(url='https://neurox-webhook-bot.onrender.com/' + API_TOKEN)
-    app.run(host='0.0.0.0', port=5000)
+def home():
+    return "NeuroX Webhook Active", 200
